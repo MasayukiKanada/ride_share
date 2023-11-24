0f0de3779c6385c6cacb0768771543a65b5ada7a
kanada <kanada.masayuki.hata@gmail.com>
2023-11-24 09:53:43 +0900

    edit routes, controllers and views of contract

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,37 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-Route::get('user/contracts', [ContractController::class, 'index']);
+
+/*
+|-----------------------
+| 利用者側
+|-----------------------
+|
+*/Route::prefix('user') // 頭に contacts をつける
+ //->middleware(['auth']) // 認証
+ ->name('contracts') // ルート名
+ ->controller(ContractController::class) // コントローラ指定(laravel9から)
+ ->group(function(){ // グループ化
+ Route::get('/contracts', 'index')->name('user.contracts'); // 名前つきルート
+});
+
+//Route::get('user/contracts', [ContractController::class, 'index'])->name('user.contracts');
+
+
+/*
+|-----------------------
+| 運転者側
+|-----------------------
+|
+*/
+
+
+/*
+|-----------------------
+| 管理者側
+|-----------------------
+|
+*/
+
 
 require __DIR__.'/auth.php';
