05ea56202025eda0d7a1e1e613f5edbb73afc02b
kanada <kanada.masayuki.hata@gmail.com>
2023-11-27 11:34:32 +0900

    新規利用予約ページ編集

--- resources/views/user/contracts/create.blade.php
+++ resources/views/user/contracts/create.blade.php
@@ -9,6 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                    <p class="mb-6">{{ Auth::user()->name }}様</p>
+
                     <!-- Validation Errors -->
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
--- resources/views/user/contracts/index.blade.php
+++ resources/views/user/contracts/index.blade.php
@@ -8,6 +8,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+
+                    <p class="mb-6">{{ Auth::user()->name }}様</p>
+
                     <!------利用前履歴と利用済履歴で表示を変える-------->
 
                     <!-----利用前履歴------>
