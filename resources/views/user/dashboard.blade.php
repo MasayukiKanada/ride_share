78bb1253422cf8cfd15f2f2bdfbb3d3cd6fe25c2
kanada <kanada.masayuki.hata@gmail.com>
2023-11-27 11:50:03 +0900

    dashboardの編集

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -9,9 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div><a href="{{ route('user.contractsuser.contracts.create') }}">新規利用予約</a></div>
-                    <div><a href="{{ route('user.contractsuser.contracts') }}">利用履歴一覧</a></div>
-                    <div><a href="">マイページ</a></div>
+                    <div class="mb-6">
+                        <button class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a href="{{ route('user.contractsuser.contracts.create') }}">新規利用予約</a>
+                        </p>
+                    </div>
+                    <div class="mb-6">
+                        <button class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a href="{{ route('user.contractsuser.contracts') }}">利用履歴一覧</a>
+                        </button>
+                    </div>
+                    <div class="mb-6">
+                        <button class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            <a href="">マイページ</a>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
