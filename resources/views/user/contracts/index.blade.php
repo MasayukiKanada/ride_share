edf8a3bef161f545186dab813c81096bcd3555ec
kanada <kanada.masayuki.hata@gmail.com>
2023-11-28 10:27:02 +0900

    利用履歴一覧ページの編集

--- resources/views/user/contracts/index.blade.php
+++ resources/views/user/contracts/index.blade.php
@@ -14,47 +14,56 @@
                     <!------利用前履歴と利用済履歴で表示を変える-------->
 
                     <!-----利用前履歴------>
-                    <h3>利用前履歴</h3>
-                    @if ($before_cons->isEmpty())
-                        <p>利用前の履歴はありません。</p>
-                    @else
-                        <table>
-                            <tr>
-                                <th>利用日</th>
-                                <th>目的地</th>
-                                <th>詳細</th>
-                            </tr>
-                            @foreach($before_cons as $before_con)
-                                        <tr>
-                                            <td>{{ $before_con->con_date }}</td>
-                                            <td>{{ $before_con->con_off_place }}</td>
-                                            <td><a href="">詳細</a></td>
-                                        </tr>
-                            @endforeach
-                        </table>
-                    @endif
+                    <div class="bg-red-100 px-3 py-6">
+                        <h3 class="text-lg text-zinc-600 font-semibold">利用前履歴</h3>
+                        @if ($before_cons->isEmpty())
+                            <p>利用前の履歴はありません。</p>
+                        @else
+                            <table>
+                                <tr>
+                                    <th>利用日</th>
+                                    <th>目的地</th>
+                                    <th>詳細</th>
+                                </tr>
+                                <tr class="border-b border-gray-400">
+                                    <th class="border-r border-gray-400 py-2">利用日</th>
+                                    <th class="border-r border-gray-400 py-2">目的地</th>
+                                    <th>詳細</th>
+                                </tr>
+                                @foreach($before_cons as $before_con)
+                                            <tr class="border-b border-gray-400">
+                                                <td class="border-r border-gray-400 px-2 py-2">{{ $before_con->con_date }}</td>
+                                                <td class="border-r border-gray-400 px-2 py-2">{{ $before_con->con_off_place }}</td>
+                                                <td class="text-center bg-indigo-600 font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"><a class="block px-2 py-2" href="">詳細を見る</a></td>
+                                            </tr>
+                                @endforeach
+                            </table>
+                        @endif
+                    </div>
 
 
                     <!-----利用済履歴------>
-                    <h2>利用済履歴</h2>
-                    @if ($after_cons->isEmpty())
-                        <p>利用済の履歴はありません。</p>
-                    @else
-                        <table>
-                            <tr>
-                                <th>利用日</th>
-                                <th>目的地</th>
-                                <th>詳細</th>
-                            </tr>
-                            @foreach($after_cons as $after_con)
-                                        <tr>
-                                            <td>{{ $after_con->con_date }}</td>
-                                            <td>{{ $after_con->con_off_place }}</td>
-                                            <td><a href="">詳細</a></td>
-                                        </tr>
-                            @endforeach
-                        </table>
-                    @endif
+                    <div class="bg-gray-50 px-3 py-6">
+                        <h3 class="text-lg text-zinc-600 font-semibold">利用済履歴</h3>
+                        @if ($after_cons->isEmpty())
+                            <p>利用済の履歴はありません。</p>
+                        @else
+                            <table>
+                                <tr>
+                                    <th>利用日</th>
+                                    <th>目的地</th>
+                                    <th>詳細</th>
+                                </tr>
+                                @foreach($after_cons as $after_con)
+                                            <tr>
+                                                <td>{{ $after_con->con_date }}</td>
+                                                <td>{{ $after_con->con_off_place }}</td>
+                                                <td><a href="">詳細</a></td>
+                                            </tr>
+                                @endforeach
+                            </table>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
