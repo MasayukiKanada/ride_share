1c452d7f68040b4eb259d84bddec99cd8b689755
kanada <kanada.masayuki.hata@gmail.com>
2023-12-08 10:53:10 +0900

    オファー詳細ページの編集

--- resources/views/driver/offers/show.blade.php
+++ resources/views/driver/offers/show.blade.php
@@ -49,6 +49,9 @@
                             </div>
                         </div>
                     </div>
+                    <div class="mt-4">
+                        <a href="{{ route('driver.offers.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-gray-700">オファー履歴一覧に戻る</a>
+                    </div>
 
                 </div>
             </div>
