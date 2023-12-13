fdc7d3f4e336f5c4b41c4a02646f4cb45dfcf127
kanada <kanada.masayuki.hata@gmail.com>
2023-12-13 11:36:23 +0900

    plusCodeの説明追加

--- resources/js/base.js
+++ resources/js/base.js
@@ -1,17 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('load', function(){
 
     //パスワード表示／非表示（入力画面）
-    let eye = document.getElementById("eye");
-    eye.addEventListener('click', function () {
-        if (this.previousElementSibling.getAttribute('type') == 'password') {
-            this.previousElementSibling.setAttribute('type', 'text');
-            this.classList.toggle('fa-eye');
-            this.classList.toggle('fa-eye-slash');
-        } else {
-            this.previousElementSibling.setAttribute('type', 'password');
-            this.classList.toggle('fa-eye');
-            this.classList.toggle('fa-eye-slash');
-        }
-    })
+    // let eye = document.getElementById('eye');
 
+    // eye.addEventListener('click', function () {
+    //     if (this.previousElementSibling.getAttribute('type') == 'password') {
+    //         this.previousElementSibling.setAttribute('type', 'text');
+    //         this.classList.toggle('fa-eye');
+    //         this.classList.toggle('fa-eye-slash');
+    //     } else {
+    //         this.previousElementSibling.setAttribute('type', 'password');
+    //         this.classList.toggle('fa-eye');
+    //         this.classList.toggle('fa-eye-slash');
+    //     }
+    // });
+
+    //モーダルウィンドウ表示／非表示（plus code説明）
+    const modal_btn01 = document.querySelector('#modal-btn01');
+    const modal_btn02 = document.querySelector('#modal-btn02');
+    const modal_overlay = document.querySelector('.modal-overlay');
+    const close = document.querySelector('.close');
+
+    modal_btn01.addEventListener('click', function() {
+        modal_overlay.classList.add('active');
+        document.querySelector('body').classList.add('modal-opened'); //body要素にクラスを与える
+        close.addEventListener('click', function() {
+            modal_overlay.classList.remove('active');
+           document.querySelector('body').classList.remove('modal-opened');
+        }, false);
+    }, false);
+    modal_btn02.addEventListener('click', function() {
+        modal_overlay.classList.add('active');
+        document.querySelector('body').classList.add('modal-opened'); //body要素にクラスを与える
+        close.addEventListener('click', function() {
+            modal_overlay.classList.remove('active');
+           document.querySelector('body').classList.remove('modal-opened');
+        }, false);
+    }, false);
 });
--- resources/views/driver/contracts/show.blade.php
+++ resources/views/driver/contracts/show.blade.php
@@ -18,7 +18,7 @@
                                         <dd class="mt-1 text-sm sm:col-span-2 sm:mt-0">{{ $contract->con_date }}</dd>
                                     </div>
                                     <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                        <dt class="text-sm font-medium">乗車場所</dt>
+                                        <dt class="text-sm font-medium">乗車地のplus code（<button id="modal-btn01" class="text-blue-500 hover:text-blue-200">plus codeとは？</button>）</dt>
                                         <dd class="mt-1 text-sm sm:col-span-2 sm:mt-0">{{ $contract->con_on_place }}</dd>
                                     </div>
                                     <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -26,7 +26,7 @@
                                         <dd class="mt-1 text-sm sm:col-span-2 sm:mt-0">{{ $contract->con_on_time }}</dd>
                                     </div>
                                     <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                                        <dt class="text-sm font-medium">下車場所</dt>
+                                        <dt class="text-sm font-medium">下車地のplus code（<button id="modal-btn02" class="text-blue-500 hover:text-blue-200">plus codeとは？</button>）</dt>
                                         <dd class="mt-1 text-sm sm:col-span-2 sm:mt-0">{{ $contract->con_off_place }}</dd>
                                     </div>
                                     <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
@@ -47,6 +47,17 @@
                                     </div>
                                 </dl>
                             </div>
+                        </div>
+                    </div>
+                    <div class="modal-overlay">
+                        <div class="modal-window">
+                            <button class="close">閉じる</button>
+                            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                                plus codeとは？
+                            </h2>
+                            <p class="text-sm mb-3">plus code は、番地と同じように使えるコードです。<br>住所代わりの簡略なコードとして使用できるほか、通常の住所だけでは絞り込めない具体的な地点の指定にも役立ちます。<br>たとえば同じ建物に入口が複数ある場合に、plus code を使えば入口も含めて指定できます。</p>
+                            <p class="text-sm mb-3">plus code は緯度と経度に基づいています。<br>シンプルなグリッド方式のコードで、20 種類の英数字（混同しやすい「1」と「I」などをあらかじめ除外したもの）の組み合わせで表現されます。</p>
+                            <p class="text-sm"><a class="text-blue-500 hover:text-blue-200" href="https://support.google.com/maps/answer/7047426?hl=ja&co=GENIE.Platform%3DDesktop#:~:text=plus%20code%20%E3%81%AF%E3%80%81%E7%95%AA%E5%9C%B0%E3%81%A8,%E5%90%AB%E3%82%81%E3%81%A6%E6%8C%87%E5%AE%9A%E3%81%A7%E3%81%8D%E3%81%BE%E3%81%99%E3%80%82" target="_blank">マップヘルプ</a>より</p>
                         </div>
                     </div>
 
