79f98085f6b3280235cf4b3faf862eb08d1a32cd
kanada <kanada.masayuki.hata@gmail.com>
2023-12-14 13:48:16 +0900

    ロゴの更新

--- resources/views/layouts/driver-navigation.blade.php
+++ resources/views/layouts/driver-navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('driver.dashboard')" :active="request()->routeIs('driver.dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ダッシュボード') }}
                     </x-nav-link>
                 </div>
             </div>
--- resources/views/layouts/owner-navigation.blade.php
+++ resources/views/layouts/owner-navigation.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('owner.dashboard')" :active="request()->routeIs('owner.dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ダッシュボード') }}
                     </x-nav-link>
                 </div>
             </div>
