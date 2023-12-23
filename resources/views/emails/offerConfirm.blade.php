53a2c860ba65343a2d45855a4404c590ce603cd8
kanada <kanada.masayuki.hata@gmail.com>
2023-12-23 11:14:46 +0900

    予約／オファー完了メールのカスタマイズ

--- resources/views/emails/offerConfirm.blade.php
+++ resources/views/emails/offerConfirm.blade.php
@@ -6,7 +6,6 @@
 オファー登録を完了しましたのでお知らせします。
 下記のオファー内容をご確認ください。
 
-<x-mail::panel>
 希望提供日：{{$offer->offer_date}}<br>
 開始地点：<a class="text-blue-500" href="https://plus.codes/map/{{$offer->offer_on_place}}">{{$offer->offer_on_place}}</a><br>
 開始時間：{{$offer->offer_on_time}}<br>
@@ -14,9 +13,7 @@
 終了時間：{{$offer->offer_off_time}}<br>
 提供車種：{{$offer->offer_car}}<br>
 乗車定員：{{$offer->offer_capacity}}<br>
-希望提供料金（分／円）：{{$offer->offer_fee}}<br>
-</x-mail::panel>
-
+希望提供料金（分／円）：{{$offer->offer_fee}}<br><br>
 
 ご利用いただきましてありがとうございます。<br>
 {{ config('app.name') }}
--- resources/views/emails/reserveConfirm.blade.php
+++ resources/views/emails/reserveConfirm.blade.php
@@ -6,7 +6,6 @@
 利用予約を完了しましたのでお知らせします。
 下記の予約内容をご確認ください。
 
-<x-mail::panel>
 利用日：{{$contract->con_date}}<br>
 乗車地点：<a class="text-blue-500" href="https://plus.codes/map/{{$contract->con_on_place}}">{{$contract->con_on_place}}</a><br>
 乗車時間：{{$contract->con_on_time}}<br>
@@ -14,8 +13,7 @@
 下車時間：{{$contract->con_off_time}}<br>
 利用人数：{{$contract->con_number}}<br>
 ドライバーズランク：{{$contract->con_eva}}<br>
-利用料金：{{$contract->con_fee}}円<br>
-</x-mail::panel>
+利用料金：{{$contract->con_fee}}円<br><br>
 
 ご利用いただきましてありがとうございます。<br>
 {{ config('app.name') }}
